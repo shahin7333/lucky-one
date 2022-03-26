dc1ae45458da404a7c704cf1769490487a0aead0
shahin7333 <shaparan7333@gmail.com>
2022-03-26 16:42:55 +0600

    paragraph  updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# BEST CRICKETER WORLD
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/Components/Paragraph/Paragraph.js
+++ src/Components/Paragraph/Paragraph.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import './Paragraph.css'
 
 const Paragraph = () => {
     return (
         <div className='paragraph'>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur accusantium, aut recusandae voluptates aliquid, ut officiis sapiente corrupti, veniam in quis sunt libero ab illo quae! Odit magnam incidunt pariatur!</p>
+            <div className='react-work'>
+                <h1>How does react work?</h1>
+                <p>React is one of the most popular javascript library. Before explaning the working principle of react,at first want to know what is react? React is javascript library(not a framework) that create user interface(UIs) in a predictable and effiecient way using declarative code. That means react is a declaration. One of the biggest advantage that you can infuse HTML code with javascript. User can create a representation of DOM node by declaring the element function in react. React uses className.JSX tags have name,children and attribute. React is using JSX instead of standard javascript to simplicity components and keep code clean.</p>
             </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur accusantium, aut recusandae voluptates aliquid, ut officiis sapiente corrupti, veniam in quis sunt libero ab illo quae! Odit magnam incidunt pariatur!</p>
+            <div className='state-props'>
+                <h1>Write down props vs state.</h1>
+                <h4>props:</h4>
+                <li> props are unmutable.</li>
+                <li>You can pass properties from present components.</li>
+                <li>its can't be modified.</li>
+                <li>props are read only.</li>
+                <li>props are passed outside a components</li>
+                <h4>state:</h4>
+                <li>state are unmutable.</li>
+                <li>You can define states in the components itself.</li>
+                <li>states can be change by asynchronous.</li>
+                <li>state are pass inside a components.</li>
             </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur accusantium, aut recusandae voluptates aliquid, ut officiis sapiente corrupti, veniam in quis sunt libero ab illo quae! Odit magnam incidunt pariatur!</p>
+            <div className='use-state'>
+                <h1>How does useState work?</h1>
+                <p>useState is one kind of hook.this hook is unmutable. You can define useState in the component it self. state can be modified . It's pass data inside. After create a functional component and through some react hook at it that tracks state, can also update it and it just works.</p>
             </div>
         </div>
     );
